184dab636f256c9dd4f9cdcf8b8671be40ba6d0f
velann21 <velan@reynencourt.com>
2020-09-26 23:34:23 +0200

    Added the error base response

--- pkg/models/response/error.go
+++ pkg/models/response/error.go
@@ -3,22 +3,26 @@ package response
 import (
 	"encoding/json"
 	"github.com/sirupsen/logrus"
-	"github.com/velann21/todo-commonlib/commons/helpers"
+	"github.com/velann21/warehouse-inventory-management/pkg/helpers"
 	"net/http"
 )
 
 type ErrorResponse struct {
 	Success bool
-	Errors []Error
+	Errors  []Error
+}
+
+func NewErrorResponse()*ErrorResponse{
+	return &ErrorResponse{}
 }
 
 type Error struct {
-	Message string
+	Message   string
 	ErrorCode int
 }
 
-func (err *ErrorResponse) HandleError(er error, w http.ResponseWriter){
-	if er == nil{
+func (err *ErrorResponse) HandleError(er error, w http.ResponseWriter) {
+	if er == nil {
 		logrus.Error("invalid error")
 		return
 	}
@@ -26,40 +30,43 @@ func (err *ErrorResponse) HandleError(er error, w http.ResponseWriter){
 	switch er {
 	case helpers.InvalidRequest:
 		errObj := Error{
-			Message:er.Error(),
-			ErrorCode:1,
+			Message:   er.Error(),
+			ErrorCode: 1,
 		}
 		errList = append(errList, errObj)
 		resp := ErrorResponse{
-			Success:false,
-            Errors: errList,
+			Success: false,
+			Errors:  errList,
 		}
 		w.WriteHeader(400)
+		w.Header().Set("Content-Type", "application/json")
 		_ = json.NewEncoder(w).Encode(resp)
 
 	case helpers.SomethingWrong:
 		errObj := Error{
-			Message:er.Error(),
-			ErrorCode:1,
+			Message:   er.Error(),
+			ErrorCode: 1,
 		}
 		errList = append(errList, errObj)
 		resp := ErrorResponse{
-			Success:false,
-			Errors: errList,
+			Success: false,
+			Errors:  errList,
 		}
 		w.WriteHeader(500)
+		w.Header().Set("Content-Type", "application/json")
 		_ = json.NewEncoder(w).Encode(resp)
 	default:
 		errObj := Error{
-			Message:er.Error(),
-			ErrorCode:1,
+			Message:   er.Error(),
+			ErrorCode: 1,
 		}
 		errList = append(errList, errObj)
 		resp := ErrorResponse{
-			Success:false,
-			Errors: errList,
+			Success: false,
+			Errors:  errList,
 		}
 		w.WriteHeader(500)
+		w.Header().Set("Content-Type", "application/json")
 		_ = json.NewEncoder(w).Encode(resp)
 	}
 
